7fed431c8d2cd512af571319a8ad9fb25a3beca6
Jose Paredes - Trabajo <joseparedes@ocg.net.ve>
2022-10-04 15:59:27 -0400

    finalizado tarea2

---
+++ tarea2/index.js
@@ -0,0 +1,13 @@
+const list = new Array(
+	'Jose Paredes',
+	35,
+	true,
+	new Date(1987,8,14),
+	{
+		titulo: '¿Que hacemos con estos Musicos?',
+        autor: 'Marcos Witt',
+        fecha: new Date(1995,2,22),
+        url: 'https://www.amazon.com/-/es/Marcos-Witt-ebook/dp/B003DL3NUW',
+	},
+);
+
